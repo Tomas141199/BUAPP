eb8a09a354e353c71a6f49b9ddb6d4cb52b95fc9
Luis Alberto <70985189+Albrrrt@users.noreply.github.com>
2021-04-29 17:58:12 -0500

    refinamiento

--- resources/views/alumnos/proyeccion.blade.php
+++ resources/views/alumnos/proyeccion.blade.php
@@ -2,28 +2,9 @@
 
 
 @section('content')
-<table class="table mt-5">
-    <thead>
-        <tr>
-            <th class="bg-primary-blue text-white" scope="col">ID</th>
-            <th class="bg-primary-blue text-white" scope="col">Nombre</th>
-            <th class="bg-primary-blue text-white" scope="col">Semestre</th>
-        </tr>
-    </thead>
-    <tbody>
+    
         @foreach ($proyeccion as $datos)
         @foreach($datos as $materia)
-<<<<<<< Updated upstream
-        <tr>
-            <th scope="row">{{$materia->materia_id}}</th>
-            <td>{{$materia->nombre}}</td>
-            <td>{{$materia->semestre}}</td>
-        </tr>
-        @endforeach
-        @endforeach
-    </tbody>
-</table>
-=======
         <div class="box">
             <i class="fas fa-book-reader icon"></i>
             <h4 class="title">Materia:</h4>
@@ -38,6 +19,5 @@
 @endforeach
     </div>
 </div> 
->>>>>>> Stashed changes
 
 @endsection
